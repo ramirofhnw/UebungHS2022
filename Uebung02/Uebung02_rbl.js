d224ecb69a3dd6b3139603a20dbba2101f0350cc
ramirofhnw <114218518+ramirofhnw@users.noreply.github.com>
2022-10-04 20:23:38 +0200

    add Uebung02_rbl.js

---
+++ Uebung02/Uebung02_rbl.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// 1. Aufgabe
+
+var L = [];
+
+for (var i=1; i<100; i=i+2) {
+    L.push(i);
+}
+
+console.log(L)
+
+// 2. Aufgabe
+
+function wuerfeln() {
+    var f = [1,2,3,4,5,6];
+
+    var choice = f.length*Math.random();
+    var index = Math.floor(choice);
+    return f[index];
+}
+
+console.log(wuerfeln())
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
