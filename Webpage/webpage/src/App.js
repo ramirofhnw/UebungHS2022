377f7c7c1990ae47e31cbfcd3a376f7cab31000f
ramirofhnw <114218518+ramirofhnw@users.noreply.github.com>
2022-12-22 13:22:46 +0100

    finale?

--- Webpage/webpage/src/App.js
+++ Webpage/webpage/src/App.js
@@ -9,13 +9,15 @@ import Typography from '@mui/material/Typography';
 import "./Karte.css";
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
-import IconButton from '@mui/material/IconButton';
+
+
 //import { MenuIcon } from '@mui/material/MenuItem';
+//npm install @mui/icons-material
 
 
 function App() {
-  const [lat, setLatitude] = useState(47.54268170654611);
-  const [lng, setLongtitude] = useState(7.593689481072517);
+  const [lat, setLatitude] = useState(47.53486);
+  const [lng, setLongtitude] = useState(7.64193);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -48,86 +50,74 @@ function App() {
   }
 
   function karte() {
-/*
-    React.useEffect(() => {
-      const L = require("leaflet");
-  
-      delete L.Icon.Default.prototype._getIconUrl;
-  
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-        iconUrl: require("leaflet/dist/images/marker-icon.png"),
-        shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-      });
-    }, []);
-  */  
     var swisstopo = (<TileLayer url = "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg"
                     attribution='&copy; swisstopo'></TileLayer>);
 
                     return (
-                      <MapContainer center={[lat, lng]} zoom={15} scrollWheelZoom={true}>
+                      <MapContainer  center={[lat, lng]} zoom={15} scrollWheelZoom={false}>
                       
                       {swisstopo}
-                      
-                    
+                                         
                       <Marker position={[lat, lng]}>
                         <Popup>
-                          <b>umgerechnete Koordinate</b><br/><div>{data?.easting}</div><div>{data?.northing}</div>
+                          <b>LV95 Koordinaten</b><br/><div>{(Math.round((data?.easting)*1000))/1000}</div><div>{(Math.round((data?.northing)*1000))/1000}</div>
                         </Popup>
                       </Marker>
-                    
+                      
                     </MapContainer>
                       );
   }
   return  <>
           <AppBar position="static">
             <Toolbar variant="dense">
-              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                
-              </IconButton>
-              <Typography variant="h6" color="inherit" component="div">
-                Umrechner WGS84 zu LV95
+              <Typography variant="h5" color="inherit" component="div">
+                Umrechner WGS84 zu LV95 Koordinaten
               </Typography>
             </Toolbar>
           </AppBar>
           <br></br>     
           <Grid container spacing={2}>
-            <Grid item xs={6}>
+            <Grid item xs={2}>
               <TextField label="Breite" variant="outlined" defaultValue={lat} onChange={ (event) => {setLatitude(event.target.value)}}/>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={2}>
               <TextField label="Länge" variant="outlined" defaultValue={lng} onChange={ (event) => {setLongtitude(event.target.value)}}/>
             </Grid>
-            <Grid item xs={12}>
-              <Button variant="contained" onClick={ () => { download()} }>Calculate</Button>
+            <Grid item xs={8}>
+              <Button color="error" style={{float: 'right'}} as="a" href="https://www.swisstopo.admin.ch/de/wissen-fakten/geodaesie-vermessung/koordinaten/bezugssystem.html" target="blank" variant="contained" onClick={ () => { download()} }>Informationen WGS84</Button>
             </Grid>
             <Grid item xs={6}>
-              <TextField label="East" variant="outlined" defaultValue="East" value={data?.easting}/>
+              <Button variant="contained" onClick={ () => { download()} }>berechnen</Button>
             </Grid>
             <Grid item xs={6}>
-              <TextField label="North" variant="outlined" defaultValue="North" value={data?.northing}/>
+              {loading &&
+                <h4>Bitte warten.... Die Daten werden geladen!!!</h4>
+              }
+
+              {error &&
+                <h4>Fehler... Server Problem!!</h4>
+              }
+
+              {data &&
+                <>
+                  <h4>Daten geladen</h4>
+                  
+                </>
+              }
+            </Grid>
+            <Grid item xs={2}>
+              <TextField label="East" variant="outlined" defaultValue="East" value={(Math.round((data?.easting)*1000))/1000}/>
+            </Grid>
+            <Grid item xs={2}>
+              <TextField label="North" variant="outlined" defaultValue="North" value={(Math.round((data?.northing)*1000))/1000}/> 
+            </Grid>
+            <Grid item xs={8}> 
+              <Button color="error" style={{float: 'right'}} as="a" href="https://www.swisstopo.admin.ch/de/wissen-fakten/geodaesie-vermessung/bezugsrahmen/lokal/lv95.html" target="blank" variant="contained" onClick={ () => { download()} }>Informationen LV95</Button>
             </Grid>
             <Grid item xs={12} onChange={ (event) => {setLatitude(event.target.value) ; setLongtitude(event.target.value)}}>
             {
             karte()
             }
-            </Grid>
-            <Grid>
-              {loading &&
-                <h1>Bitte warten.... Die Daten werden geladen!!!</h1>
-              }
-
-              {error &&
-                <h1>Fehler... Server Problem!!</h1>
-              }
-
-              {data &&
-                <>
-                  <h1>Daten geladen</h1>
-                  <div>{data?.easting}</div>
-                  <div>{data?.northing}</div>
-                </>
-              }
             </Grid>
           </Grid>   
           
@@ -137,4 +127,3 @@ function App() {
 }
 
 export default App;
-
